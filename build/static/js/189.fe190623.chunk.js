d16e318c86a8e3aaf141f7301d9830feb21d5ab9
devanjaliraj <anjalidevelopersveltosest@gmail.com>
2023-01-22 15:08:29 +0530

    chat21/01/23

---
+++ build/static/js/189.fe190623.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[189],{1999:function(a,s){}}]);
+//# sourceMappingURL=189.fe190623.chunk.js.map
---
+++ build/static/js/2.ca9a5088.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[2],{813:function(a,s,d){}}]);
+//# sourceMappingURL=2.ca9a5088.chunk.js.map
